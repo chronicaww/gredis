02099cccaa03d7a98841033fae738e4bfd99f720
chronicaww <chronicle2nd_ww@hotmail.com>
2016-07-04 11:15:32 +0900

    test

--- gredis_test.go
+++ gredis_test.go
@@ -1,7 +1,7 @@
 package gredis
 
 import (
-	// "fmt"
+	"fmt"
 	"testing"
 )
 
@@ -11,49 +11,73 @@ import (
 // 	for _, v := range result {
 // 		fmt.Printf("%v ", ToInt32(v))
 // 	}
-// 	fmt.Println()
+// 	fmt.Println("121")
 // 	if len(result) != 20 {
 // 		t.Error("Error:", result)
 // 	}
 // }
 
-func Test_List(t *testing.T) {
+func Test_Rank(t *testing.T) {
 	Instance(1)
 	FlushDB()
+	AddScore("TestScoreRank", float64(-100), []byte("No1"))
+	AddScore("TestScoreRank", float64(-90), []byte("No2"))
+	AddScore("TestScoreRank", float64(-75), []byte("No3"))
+	AddScore("TestScoreRank", float64(-70), []byte("No4"))
+	AddScore("TestScoreRank", float64(-65), []byte("No5"))
+	AddScore("TestScoreRank", float64(-60), []byte("No6"))
+	AddScore("TestScoreRank", float64(-55), []byte("No7"))
+	AddScore("TestScoreRank", float64(-44), []byte("No8"))
+	rank, e := GetRank("TestScoreRank", []byte("No7"))
 
-	list1 := [][]byte{[]byte("aaa"), []byte("bbb"), []byte("ccc"), []byte("eee")}
-	// list2 := [][]byte{[]byte("ccc"), []byte("ddd"), []byte("eee")}
-	for _, v := range list1 {
-		AddList("TestingList", string(v), -1)
-	}
+	fmt.Println("rank:", rank, e)
+	// if rank != int64(1) {
+	// 	t.Error("Error GetRank:", rank)
+	// }
+	// list, _ := GetRangeByRank("TestScoreRank", 0, 7)
+	// for _, v := range list {
+	// 	fmt.Println("name", ToString(v))
+	// }
 
-	if len(GetList("TestingList")) != 4 {
-		t.Error("Error AddList1:", len(GetList("TestingList")))
-	}
-
-	e := AddList("TestingList", "add", 1)
-	if e != nil {
-		t.Error("Error AddList0:", e)
-		return
-	}
-
-	pos := FindInList("TestingList", "add")
-	if pos != 1 {
-		t.Error("error pos:", pos)
-		for _, v := range GetList("TestingList") {
-			t.Error("Error AddList:", string(v))
-		}
-		return
-	}
-
-	RemListValue("TestingList", "add")
-	pos = FindInList("TestingList", "bbb")
-	if pos != 1 {
-		t.Error("Error RemListValue:", pos)
-	}
-
-	list := GetList("TestingList")
-	if len(list) != 4 {
-		t.Error("Error GetList:", list)
-	}
 }
+
+// func Test_List(t *testing.T) {
+// 	Instance(1)
+// 	FlushDB()
+//
+// 	list1 := [][]byte{[]byte("aaa"), []byte("bbb"), []byte("ccc"), []byte("eee")}
+// 	// list2 := [][]byte{[]byte("ccc"), []byte("ddd"), []byte("eee")}
+// 	for _, v := range list1 {
+// 		AddList("TestingList", string(v), -1)
+// 	}
+//
+// 	if len(GetList("TestingList")) != 4 {
+// 		t.Error("Error AddList1:", len(GetList("TestingList")))
+// 	}
+//
+// 	e := AddList("TestingList", "add", 1)
+// 	if e != nil {
+// 		t.Error("Error AddList0:", e)
+// 		return
+// 	}
+//
+// 	pos := FindInList("TestingList", "add")
+// 	if pos != 1 {
+// 		t.Error("error pos:", pos)
+// 		for _, v := range GetList("TestingList") {
+// 			t.Error("Error AddList:", string(v))
+// 		}
+// 		return
+// 	}
+//
+// 	RemListValue("TestingList", "add")
+// 	pos = FindInList("TestingList", "bbb")
+// 	if pos != 1 {
+// 		t.Error("Error RemListValue:", pos)
+// 	}
+//
+// 	list := GetList("TestingList")
+// 	if len(list) != 4 {
+// 		t.Error("Error GetList:", list)
+// 	}
+// }
